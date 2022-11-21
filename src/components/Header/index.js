d83d1512e4a1ef833ad9e00890c80c8ec6262e9c
Gustavo Teles <gustavo.teles711@gmail.com>
2022-11-21 20:32:37 -0300

    commit inicial

---
+++ src/components/Header/index.js
@@ -0,0 +1,11 @@
+import * as Styled from "./style";
+
+export default function Header() {
+  return (
+    <Styled.Container>
+      <div className="center" >
+        <h1>Ola mundo Header </h1>
+      </div>
+    </Styled.Container>
+  );
+}
